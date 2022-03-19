bdcf6d44b7980d9cb9a449ea530ceb206f08caa7
Serge Simplice Kenne <kenne_serge@mail.ru>
2022-03-19 04:26:53 +0300

    test deploy

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ connectDB()
 app.use("/api/products", productRouter)
 app.use("/api", userRouter)
 
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
